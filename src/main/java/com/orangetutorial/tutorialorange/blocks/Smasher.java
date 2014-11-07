ee4c29144fc082c1358eb8de3be496fea2dba051
haku-81 <jondouglas9000@gmail.com>
2014-11-06 19:44:01 -0800

    Fixed Issues with Tile Entity GUI

--- src/main/java/com/orangetutorial/tutorialorange/blocks/Smasher.java
+++ src/main/java/com/orangetutorial/tutorialorange/blocks/Smasher.java
@@ -1,11 +1,14 @@
 package com.orangetutorial.tutorialorange.blocks;
 
+import com.orangetutorial.tutorialorange.TutorialOrange;
+import com.orangetutorial.tutorialorange.tileentities.GUIs;
 import com.orangetutorial.tutorialorange.tileentities.TileEntitySmasher;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 
@@ -20,6 +23,18 @@ public class Smasher extends Block implements ITileEntityProvider {
         setCreativeTab(CreativeTabs.tabBlock);
         GameRegistry.registerBlock(this, privateName);
     }
+    @Override
+    public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int meta, float hitX, float hitY, float hitZ) {
+        if (world.isRemote) {
+            if (world.getTileEntity(x, y, z) != null)
+                player.openGui(TutorialOrange.instance, GUIs.SMASHER.ordinal(), world, x, y, z);
+            return true;
+        }
+        return true;
+    }
+
+
+
 
     @Override
     public TileEntity createNewTileEntity(World world, int meta) {
--- src/main/java/com/orangetutorial/tutorialorange/tileentities/TileEntitySmasher.java
+++ src/main/java/com/orangetutorial/tutorialorange/tileentities/TileEntitySmasher.java
@@ -11,15 +11,7 @@ public class TileEntitySmasher extends TileEntity {
 
     public static String name = "tileEntitySmasher";
 
-    @Override
-    public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int meta, float hitX, float hitY, float hitZ) {
-        if (world.isRemote) {
-            if (world.getTileEntity(x, y, z) != null)
-                player.openGui(TutorialOrange.instance, GUIs.SMASHER.ordinal(), world, x, y, z);
-            return true;
-        }
-        return true;
-    }
+
 
 
 }
